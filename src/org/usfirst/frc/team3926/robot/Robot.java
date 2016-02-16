4c0ceebb24846f79ac0361ba472d375d8932b539
Brookshank <joe.brooksbank@gmail.com>
2016-02-16 16:52:46 -0600

    Added Camera; Slowed speed

--- src/org/usfirst/frc/team3926/robot/Robot.java
+++ src/org/usfirst/frc/team3926/robot/Robot.java
@@ -5,6 +5,13 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Timer;
+
 
 
 public class Robot extends IterativeRobot {
@@ -17,6 +24,10 @@ public class Robot extends IterativeRobot {
     double leftInput;
     Joystick rightStick;
     double rightInput;
+    
+    int session;
+    Image frame;
+    CameraServer server;
    
     public void robotInit() {
        talon_FL = new Talon(0);
@@ -27,23 +38,35 @@ public class Robot extends IterativeRobot {
        leftStick = new Joystick(0);
        rightStick = new Joystick(1);
     
+       session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+       NIVision.IMAQdxConfigureGrab(session);
+       frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
     }
    
     public void teleopPeriodic() {
     	leftInput = leftStick.getY();
     	rightInput = rightStick.getY();
-    	
+    	leftInput /= 2;
+    	rightInput /=2;
     	driveSystem.tankDrive(leftInput, rightInput);
     	
     	 	if (rightStick.getRawButton(1)){
     	 		leftInput = rightInput;
     	 	}
-    	 	if (leftStick.getRawButton(1)){
-    	 		leftInput /= 2;
-    	 		rightInput /= 2;
-    	 	}
+    	 	webcam();
+    	 	
     }
     
+    
+    public void webcam() { 
+    	NIVision.Rect rect = new NIVision.Rect(200, 250, 100, 100);
+
+        NIVision.IMAQdxGrab(session, frame, 1);
+        NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+        
+        CameraServer.getInstance().setImage(frame);
+        Timer.delay(0.005);
+    } 
  
   
 }
